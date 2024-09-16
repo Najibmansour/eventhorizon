4f8b1dba63226daf75fe2aa0a56d40451e9d6f58
Najibmansouur <najibmansour2005@gmail.com>
2024-09-16 23:07:37 +0300

    CREATE: eventposts controller ADD: index function

--- app/Http/Controllers/API/EventPostController.php
+++ app/Http/Controllers/API/EventPostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\EventPost;
 use Illuminate\Http\Request;
 
 class EventPostController extends Controller
@@ -12,7 +13,7 @@ class EventPostController extends Controller
      */
     public function index()
     {
-        //
+        EventPost::all();
     }
 
     /**
